21680ad3a7b011cc0f111428d37c3790c4420039
richjimdev <richjimdev@gmail.com>
2018-09-17 12:24:26 -0700

    radio buttons

--- scripts/app.js
+++ scripts/app.js
@@ -7,3 +7,15 @@ $('.spriteCheckBox').on('click', () => {
   $(event.target).toggleClass('checkbox-checked');
   $(event.target).toggleClass('checkbox-empty');
 });
+
+$('#r1').on('click', () => {
+  console.log('clicked!!');
+  $('#r1').removeClass('radio-empty').addClass('radio-checked');
+  $('#r2').removeClass('radio-checked').addClass('radio-empty');
+});
+
+$('#r2').on('click', () => {
+  console.log('clicked!!');
+  $('#r2').removeClass('radio-empty').addClass('radio-checked');
+  $('#r1').removeClass('radio-checked').addClass('radio-empty');
+});
